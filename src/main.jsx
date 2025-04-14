2116678c8f0852abda1a1a358021558d386086f2
vishal pathak <vishalpathak842@gmail.com>
2025-04-15 02:15:34 +0530

    remove commented-out code in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -9,15 +9,3 @@ createRoot(document.getElementById('root')).render(
   </StrictMode>,
 )
 
-// src/index.js
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import App from './App';
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
